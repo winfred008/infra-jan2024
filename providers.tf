5abb408387fe01da967c536775d4b3dc0aa70d16
winfred008 <144569892+winfred008@users.noreply.github.com>
2024-02-13 12:32:33 -0500

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,4 +1,12 @@
-
+terraform {
+  backend "s3" {
+    bucket = "winfred008-terraform-state"
+    key    = "global/s3/terraform.tfstate"
+    region = "us-east-2"
+    dynamodb_table = "terraform-state"
+    encrypt = true
+  }
+}
 
 # Configure the AWS Provider
 provider "aws" {

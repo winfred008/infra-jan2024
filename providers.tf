41714e4af14a87dfbc04c447a7694a887adedf07
winfred008 <144569892+winfred008@users.noreply.github.com>
2024-02-13 12:34:29 -0500

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -3,7 +3,7 @@ terraform {
     bucket = "winfred008-terraform-state"
     key    = "global/s3/terraform.tfstate"
     region = "us-east-2"
-    dynamodb_table = "terraform-state"
+    dynamo_table = "terraform-state"
     encrypt = true
   }
 }

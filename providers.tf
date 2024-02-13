7893508b5bef6a08d5b8a24c6484ba76dc0c9547
winfred008 <144569892+winfred008@users.noreply.github.com>
2024-02-13 14:26:42 -0500

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "winfred008"
+    bucket = "winfred008-terraform-state"
     key    = "global/s3/terraform.tfstate"
     region = "us-east-2"
     dynamodb_table = "terraform-state"

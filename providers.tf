a4b8172cc1856f880148eaab3fdbbe9fc41fa32f
winfred008 <144569892+winfred008@users.noreply.github.com>
2024-02-13 12:46:19 -0500

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket = "winfred008-terraform-state"
     key    = "global/s3/terraform.tfstate"
-    region = "us-east-2"
+    region = "us_east_2"
     dynamo_table = "terraform-state"
     encrypt = true
   }
@@ -10,5 +10,5 @@ terraform {
 
 # Configure the AWS Provider
 provider "aws" {
-  region = "us-east-2"
+  region = "us_east_2"
 }
